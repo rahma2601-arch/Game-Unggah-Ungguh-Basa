4679b0c4d2aa74393b60053928592ab10f77add3
rahma2601-arch <rahmaagiba2601@students.unnes.ac.id>
2026-05-04 00:44:49 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6aHR0tsdyCb":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="game.mp3";
+audio.load0;
+audio.play0;
+}
+
